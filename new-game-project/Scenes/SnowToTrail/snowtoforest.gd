053ca2c3a024207abf3c22bbd3cfd2a9cc3d2190
varun chaaras <chaarasvarun@gmail.com>
2025-03-27 14:33:24 +0530

    lights fixed

---
+++ new-game-project/Scenes/SnowToTrail/snowtoforest.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		PlayerManager.save_position()
+		get_tree().change_scene_to_file("res://Scenes/giantForest/forest.tscn")
