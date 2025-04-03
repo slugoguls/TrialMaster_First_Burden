e18ccc56da17d6f84b8efe606cef264f498d3fbe
varun chaaras <chaarasvarun@gmail.com>
2025-04-03 15:19:47 +0530

    THE GATYEEEEEEEE

---
+++ new-game-project/Scenes/the gate/the_gate.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var marker_from_Town = $FromTown
+@onready var marker_from_TrialGround= null
+
+func _ready():
+	var spawn_position: Vector2
+	if PlayerManager.previous_location == "Town":
+		spawn_position = marker_from_Town.global_position
+	elif PlayerManager.previous_location == "TrialGround":
+		spawn_position = marker_from_TrialGround.global_position
+	else:
+		spawn_position = marker_from_Town.global_position # Fallback
+
+	PlayerManager.spawn_player(self, spawn_position)
--- new-game-project/Scenes/waterfall/waterfall.gd
+++ new-game-project/Scenes/waterfall/waterfall.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 @onready var marker_from_trail = $fromTrail
-@onready var marker_from_ghostTown = null
+@onready var marker_from_ghostTown = $fromTown
 
 func _ready():
 	var spawn_position: Vector2
 	if PlayerManager.previous_location == "trail":
 		spawn_position = marker_from_trail.global_position
-	elif PlayerManager.previous_location == "ghostTown":
+	elif PlayerManager.previous_location == "Town":
 		spawn_position = marker_from_ghostTown.global_position
 	else:
 		spawn_position = marker_from_trail.global_position # Fallback
