653945e6e5a1d4e0861739f7a640ad1c7fec38ed
varun chaaras <chaarasvarun@gmail.com>
2025-04-07 13:30:49 +0530

    level designed

--- new-game-project/Scenes/abyssToTrail/toAbyssfromTrial.gd
+++ new-game-project/Scenes/abyssToTrail/toAbyssfromTrial.gd
@@ -3,6 +3,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		PlayerManager.previous_location = "trail"
+		PlayerManager.previous_location = "trial"
 		PlayerManager.save_position()
 		get_tree().change_scene_to_file("res://Scenes/entryAbyss/entry_abyss.tscn")
